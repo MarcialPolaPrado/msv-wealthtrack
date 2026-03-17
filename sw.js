761b48ab1b5fd4de02abc94d188423e7f162ce5a
MarcialPolaPrado <marcialpolaprado@gmail.com>
2026-03-17 08:14:55 +0100

    Actualizacion automatica

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
-const CACHE_NAME = 'msv-wealthtrack-v202603170811';
+const CACHE_NAME = 'msv-wealthtrack-v202603170814';
 const ASSETS = [
     './',
     './index.html',
     './styles.css',
-    './app.js?v=202603170811',
+    './app.js?v=202603170814',
     './storage.js?v=202603161637',
     './mock_data.js?v=202603161637',
     './manifest.json',
